65a14e5a42a6598995feb9280db6d01417402ab6
Ranjan Shrestha <ranjan@sharingapples.com>
2018-06-04 17:35:23 +0545

    Add a prop to pass transition duration

--- packages/navigator-native/src/Navigator.js
+++ packages/navigator-native/src/Navigator.js
@@ -7,23 +7,36 @@ function createDriver() {
   return new Animated.Value(0);
 }
 
-function runDriver(driver) {
-  return new Promise((resolve) => {
-    Animated.timing(driver, {
-      toValue: 1,
-      useNativeDriver: true,
-      duration: 400,
-      easing: Easing.bezier(0.25, 0.46, 0.45, 0.94),
-    }).start((result) => {
-      if (!result.finished) {
-        driver.setValue(1);
-      }
+function createRunDriver(duration) {
+  return function runDriver(driver) {
+    return new Promise((resolve) => {
+      Animated.timing(driver, {
+        toValue: 1,
+        useNativeDriver: true,
+        duration,
+        easing: Easing.bezier(0.25, 0.46, 0.45, 0.94),
+      }).start((result) => {
+        if (!result.finished) {
+          driver.setValue(1);
+        }
 
-      resolve(result);
+        resolve(result);
+      });
     });
-  });
+  };
 }
 
-export default function (props) {
-  return <Navigator {...props} createDriver={createDriver} runDriver={runDriver} />;
+type Props = {
+  duration: number,
+};
+
+export default function (props: Props) {
+  const { duration, ...other } = props;
+  return (
+    <Navigator
+      {...other}
+      createDriver={createDriver}
+      runDriver={createRunDriver(duration || 400)}
+    />
+  );
 }
