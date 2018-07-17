1a54f23ae30ec4156504c46e3370db38a6c3e92e
Ranjan Shrestha <ranjan@sharingapples.com>
2018-07-17 22:00:16 +0545

    Add MenuCard and MenuScreen

---
+++ packages/menu/src/MenuCard.js
@@ -0,0 +1,137 @@
+// @flow
+import React, { Component } from 'react';
+import { withNavigator, createTracker } from '@bhoos/navigator-native';
+import { View, Animated, TouchableWithoutFeedback, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+type Props = {
+  navigator: Object,
+  style: Object,
+  onSelect: (navigator: Object) => void,
+  tilt: number,
+  anchorY: number,
+  route: string,
+  scale: {
+    x: number,
+    y: number,
+  },
+};
+
+class MenuCard extends Component<Props> {
+  constructor(props) {
+    super(props);
+
+    this.translateY = new Animated.Value(0);
+    this.tilt = new Animated.Value(0);
+    this.flip = new Animated.Value(0);
+
+    this.scaleX = new Animated.Value(1);
+    this.scaleY = new Animated.Value(1);
+
+    const rotationY = props.tilt >= 0 ? ['0deg', '360deg'] : ['360deg', '0deg'];
+    const rotationZ = props.tilt >= 0 ? ['0deg', '180deg'] : ['0deg', '-180deg'];
+
+    this.style = {
+      opacity: this.flip.interpolate({
+        inputRange: [0, 90, 90, 180],
+        outputRange: [1, 1, 0, 0],
+      }),
+      transform: [
+        { perspective: 1200 },
+        { translateY: this.translateY },
+        {
+          rotateY: this.flip.interpolate({
+            inputRange: [0, 360],
+            outputRange: rotationY,
+          }),
+        },
+        {
+          rotate: this.flip.interpolate({
+            inputRange: [0, 360],
+            outputRange: rotationZ,
+          }),
+        },
+        { translateY: props.anchorY },
+        {
+          rotate: this.tilt.interpolate({
+            inputRange: [0, 360],
+            outputRange: ['0deg', '360deg'],
+          }),
+        },
+        { translateY: -props.anchorY },
+        { scaleX: this.scaleX },
+        { scaleY: this.scaleY },
+      ],
+    };
+
+    this.tracker = createTracker({ });
+  }
+
+  onEnter(transition) {
+    if (transition.outgoing !== this.props.route) {
+      this.translateY.setValue(400);
+      this.tracker.track(this.translateY, transition, 0);
+    } else {
+      this.tilt.setValue(-this.props.tilt);
+      this.translateY.setValue(0);
+      this.flip.setValue(180);
+
+      const { scale } = this.props;
+      this.scaleX.setValue(scale.x);
+      this.scaleY.setValue(scale.y);
+      this.tracker.track(this.scaleX, transition, 1);
+      this.tracker.track(this.scaleY, transition, 1);
+    }
+
+    this.tracker.track(this.tilt, transition, this.props.tilt);
+    this.tracker.track(this.flip, transition, 0);
+  }
+
+  onLeave(transition) {
+    if (transition.incoming !== this.props.route) {
+      this.tracker.track(this.translateY, transition, 400);
+      this.tracker.track(this.tilt, transition, 0);
+    } else {
+      const { scale } = this.props;
+
+      this.tracker.track(this.flip, transition, 180);
+      this.tracker.track(this.scaleX, transition, scale.x);
+      this.tracker.track(this.scaleY, transition, scale.y);
+
+      this.tracker.track(this.tilt, transition, -this.props.tilt);
+    }
+  }
+
+  render() {
+    const {
+      navigator, style, onSelect, tilt, route, scale, ...other
+    } = this.props;
+
+    return (
+      <View style={[StyleSheet.absoluteFill, styles.container]} pointerEvents="box-none">
+        <TouchableWithoutFeedback
+          onPressIn={() => {
+            console.log('Pressed', route);
+            onSelect(navigator);
+          }}
+        >
+          <Animated.View
+            style={[styles.container, this.style]}
+            {...other}
+            renderToHardwareTextureAndroid
+          />
+        </TouchableWithoutFeedback>
+      </View>
+    );
+  }
+}
+
+export default withNavigator(MenuCard);
+
---
+++ packages/menu/src/MenuScreen.js
@@ -0,0 +1,92 @@
+// @flow
+import React, { Component } from 'react';
+import { View, Animated, TouchableWithoutFeedback } from 'react-native';
+import { createTracker } from '@bhoos/navigator-native';
+
+type Props = {
+  navigator: Object,
+};
+
+export default function createMenuScreen(Target, { style, invScale, clockWise, dimension }) {
+  return class MenuScreen extends Component<Props> {
+    constructor(props) {
+      super(props);
+
+      this.scale = {
+        x: 1 / invScale.x,
+        y: 1 / invScale.y,
+      };
+
+      this.flip = new Animated.Value(180);
+      this.scaleX = new Animated.Value(this.scale.y);
+      this.scaleY = new Animated.Value(this.scale.x);
+
+      const rotationY = clockWise ? ['0deg', '360deg'] : ['360deg', '0deg'];
+      const rotationZ = clockWise ? ['180deg', '0deg'] : ['-180deg', '0deg'];
+
+      this.style = {
+        opacity: this.flip.interpolate({
+          inputRange: [0, 90, 90, 180, 270, 270, 360],
+          outputRange: [1, 1, 0, 0, 0, 1, 1],
+        }),
+        transform: [
+          { perspective: 1200 },
+          {
+            rotateY: this.flip.interpolate({
+              inputRange: [0, 360],
+              outputRange: rotationY,
+            }),
+          },
+          {
+            rotate: this.flip.interpolate({
+              inputRange: [0, 360],
+              outputRange: rotationZ, // ['180deg', '0deg'],
+            }),
+          },
+          { scaleX: this.scaleX },
+          { scaleY: this.scaleY },
+        ],
+      };
+
+      this.tracker = createTracker({ });
+    }
+
+    onEnter(transition) {
+      this.tracker
+        .track(this.flip, transition, 360)
+        .track(this.scaleX, transition, 1)
+        .track(this.scaleY, transition, 1);
+    }
+
+    onLeave(transition) {
+      this.tracker
+        .track(this.flip, transition, 180)
+        .track(this.scaleX, transition, this.scale.y)
+        .track(this.scaleY, transition, this.scale.x);
+    }
+
+    render() {
+      return (
+        <Animated.View style={[style, this.style]} renderToHardwareTextureAndroid>
+          <Target />
+          <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+            <TouchableWithoutFeedback onPressIn={() => this.props.navigator.setRoute('home')}>
+              <View
+                style={{
+                  position: 'absolute',
+                  left: (dimension.width / 2) - 20,
+                  top: -dimension.height - 20,
+                  width: 40,
+                  height: 40,
+                  borderRadius: 20,
+                  backgroundColor: 'green',
+                }}
+              />
+            </TouchableWithoutFeedback>
+          </View>
+        </Animated.View>
+      );
+    }
+  };
+}
+
